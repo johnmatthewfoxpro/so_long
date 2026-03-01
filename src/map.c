fc01d5fa06e71cadbe544d1aeb3215c21a29086e
johnmatthewfoxpro <johnmatthewfoxpro@gmail.com>
2026-03-01 15:19:07 +0100

    Progress made continuing with parsing. Now all checks have been made, only flood fill remains before we can start rendering the map.

--- src/map.c
+++ src/map.c
@@ -6,37 +6,13 @@
 /*   By: jfox <jfox.42angouleme@gmail.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 18:26:56 by jfox              #+#    #+#             */
-/*   Updated: 2026/02/28 19:57:15 by jfox             ###   ########.fr       */
+/*   Updated: 2026/03/01 14:51:44 by jfox             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header/solong.h"
 
-
-// check known collum number against every row. Throw error if not equal.
-// check if map is square.
-void	check_grid(t_game *so_long)
-{
-	int	y;
-
-	y = 0;
-	while (so_long->map[y])
-	{
-		if ((int)ft_strlen(so_long->map[y]) != so_long->collums)
-		{
-			free_vals(so_long);
-			map_errors(-4);
-		}
-		if ((int)ft_strlen(so_long->map[y]) == so_long->rows)
-		{
-			free_vals(so_long);
-			map_errors(-5);
-		}
-		y++;
-	}
-}
-
-void	check_characters(t_game *so_long)
+static void	check_elements(t_game *so_long)
 {
 	int	y;
 	int	x;
@@ -47,22 +23,26 @@ void	check_characters(t_game *so_long)
 		x = 0;
 		while (so_long->map[y][x])
 		{
-			if (so_long->map[y][x] != '0' && so_long->map[y][x] != '1'
-				&& so_long->map[y][x] != 'C' && so_long->map[y][x] != 'E'
-				&& so_long->map[y][x] != 'P')
-			{
-				free_vals(so_long);
-				map_errors(-6);
-			}
+			if (so_long->map[y][x] == 'E')
+				so_long->exit += 1;
+			else if (so_long->map[y][x] == 'P')
+				so_long->start += 1;
+			else if (so_long->map[y][x] == 'C')
+				so_long->collect += 1;
 			x++;
 		}
 		y++;
+	}
+	if (so_long->exit != 1 || so_long->start != 1 || so_long->collect < 1)
+	{
+		free_vals(so_long);
+		element_errors(so_long);
 	}
 }
 
 // check valid map format '.ber'
 // turn map arument into a single string.
-// FInd number of collums with strlen then use i to find number of rows.
+// Find number of collums with strlen then use i to find number of rows.
 // Check collums are equal. Then check all characters on map are valid.
 void	read_map(char *map, t_game *so_long)
 {
@@ -72,17 +52,25 @@ void	read_map(char *map, t_game *so_long)
 	if (!map || !map_format(map))
 		main_errors(-2);
 	so_long->map = build_map(map);
-	// ft_printf("%s\n",so_long->map[0]);
-	// ft_printf("%s\n",so_long->map[1]);
-	// ft_printf("%s\n",so_long->map[2]);
-	// ft_printf("%s\n",so_long->map[3]);
-	// ft_printf("%s\n",so_long->map[4]);
-	// ft_printf("%s\n",so_long->map[5]);
 	so_long->collums = ft_strlen(so_long->map[i]);
 	while (so_long->map[i])
 		i++;
 	so_long->rows = i;
 	check_grid(so_long);
 	check_characters(so_long);
+	check_walls(so_long);
+	check_elements(so_long);
+	//flood_fill(so_long);
 	return ;
 }
+
+	// ft_printf("%s\n",so_long->map[0]);
+	// ft_printf("%s\n",so_long->map[1]);
+	// ft_printf("%s\n",so_long->map[2]);
+	// ft_printf("%s\n",so_long->map[3]);
+	// ft_printf("%s\n",so_long->map[4]);
+	// ft_printf("%s\n",so_long->map[5]);
+
+	// ft_printf("%d\n", so_long->exit);
+	// ft_printf("%d\n", so_long->start);
+	// ft_printf("%d\n", so_long->collect);
